b8b8412b220749400e08ffd6a2af058c5e7d5f8e
tkermit364-ctrl <tkermit364@gmail.com>
2026-04-22 15:08:17 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+function goToCourse(course) {
+  localStorage.setItem("currentCourse", course);
+  window.location.href = "course.html";
+}
+
+function goBack() {
+  window.location.href = "index.html";
+}
+
+const courseData = {
+  math: ["Algebra Homework", "Geometry Worksheet"],
+  science: ["Lab Report", "Read Chapter 3"],
+  history: ["Essay on WWII", "Timeline Project"]
+};
+
+if (document.getElementById("course-title")) {
+  const course = localStorage.getItem("currentCourse");
+
+  document.getElementById("course-title").innerText = course;
+
+  const list = document.getElementById("assignments");
+
+  courseData[course].forEach(item => {
+    const li = document.createElement("li");
+    li.innerText = item;
+    list.appendChild(li);
+  });
+}
